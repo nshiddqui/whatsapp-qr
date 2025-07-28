ada0d3400587e7ad141c8d7f5689cac36e941293
Nazim <nazim@adaptabiz.com>
2025-07-29 02:37:45 +0530

    commit

--- src/Socket/messages-send.ts
+++ src/Socket/messages-send.ts
@@ -28,7 +28,9 @@ import {
 	getWAUploadToServer,
 	normalizeMessageContent,
 	parseAndInjectE2ESessions,
-	unixTimestampSeconds
+	unixTimestampSeconds,
+	generateWAMessageFromContent,
+	prepareWAMessageMedia
 } from '../Utils'
 import { getUrlInfo } from '../Utils/link-preview'
 import {
@@ -629,7 +631,7 @@ export const makeMessagesSocket = (config: SocketConfig) => {
 			}
 
 			if (shouldIncludeDeviceIdentity) {
-				;(stanza.content as BinaryNode[]).push({
+				; (stanza.content as BinaryNode[]).push({
 					tag: 'device-identity',
 					attrs: {},
 					content: encodeSignedDeviceIdentity(authState.creds.account!, true)
@@ -639,7 +641,7 @@ export const makeMessagesSocket = (config: SocketConfig) => {
 			}
 
 			if (additionalNodes && additionalNodes.length > 0) {
-				;(stanza.content as BinaryNode[]).push(...additionalNodes)
+				; (stanza.content as BinaryNode[]).push(...additionalNodes)
 			}
 
 			logger.debug({ msgId }, `sending message to ${participants.length} devices`)
@@ -871,6 +873,58 @@ export const makeMessagesSocket = (config: SocketConfig) => {
 
 				return fullMsg
 			}
+		},
+		sendIAMessage: async (jid: string, btns = [], quoted, opts = {}) => {
+			let messageContent = {
+				viewOnceMessage: {
+					message: {
+						interactiveMessage: proto.Message.InteractiveMessage.create({
+							body: proto.Message.InteractiveMessage.Body.create({
+								text: opts.content, // Isi utama pesan
+							}),
+							footer: proto.Message.InteractiveMessage.Footer.create({
+								text: opts.footer, // Footer pesan
+							}),
+							header: proto.Message.InteractiveMessage.Header.create({
+								title: opts.header, // Header pesan
+								subtitle: '',
+								hasMediaAttachment: !!opts.media, // Apakah ada media?
+							}),
+							contextInfo: {
+								forwardingScore: 9999, // Nilai forward (opsional)
+								isForwarded: false,
+								mentionedJid: conn.parseMention(opts.header + opts.content + opts.footer),
+							},
+							externalAdReply: {
+								showAdAttribution: true,
+								renderLargerThumbnail: false,
+								mediaType: 1, // Media tipe (default: gambar)
+							},
+							nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
+								buttons: btns, // Tombol interaktif
+							}),
+						}),
+					},
+				},
+			};
+
+			// Tambahkan media jika diperlukan
+			if (opts.media) {
+				const media = await prepareWAMessageMedia(
+					{
+						[opts.mediaType || 'image']: { url: opts.media },
+					},
+					{ upload: conn.waUploadToServer }
+				);
+				messageContent.viewOnceMessage.message.interactiveMessage.header.hasMediaAttachment = true;
+				Object.assign(messageContent.viewOnceMessage.message.interactiveMessage.header, media);
+			}
+
+			// Buat pesan dan kirimkan
+			let msg = await generateWAMessageFromContent(jid, messageContent, { quoted });
+			await relayMessage(msg.key.remoteJid, msg.message, {
+				messageId: msg.key.id,
+			});
 		}
 	}
 }
